1755328275771f40ce52b8666c4f0fc8bbc6017b
koyamanX <ckoyama.1996@gmail.com>
2023-02-15 21:55:59 +0900

    Add strcmp

--- string.c
+++ string.c
@@ -39,3 +39,14 @@ size_t strlen(char *s) {
 
 	return cnt;
 }
+
+int strcmp(const char *s1, const char *s2) {
+	while(*s1) {
+		if(*s1 != *s2) {
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return *((const unsigned char *) s1) - *((const unsigned char *) s2);
+}
--- string.h
+++ string.h
@@ -7,4 +7,7 @@ char *strcpy(char *dest, const char *src);
 void *memset(void *s, int c, u64 sz);
 size_t strlen(char *s);
 void *memcpy(void *dest, const void *src, size_t n);
+
+int strcmp(const char *s1, const char *s2);
+
 #endif

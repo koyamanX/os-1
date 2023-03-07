a1b28b8de375f7190280f12decdb2fb0139e4cf3
koyamanX <ckoyama.1996@gmail.com>
2023-03-07 22:26:42 +0900

    WIP add exec

--- libc/include/unistd.h
+++ libc/include/unistd.h
@@ -8,5 +8,6 @@ ssize_t write(int fd, const void *buf, size_t count);
 ssize_t read(int fd, const void *buf, size_t count);
 int dup(int fildes);
 int fork(void);
+int exec(const char *pathname, const char *argv[]);
 
 #endif

f4b92953981881a8ca7d6c441c5b6dee124cc0a0
koyamanX <ckoyama.1996@gmail.com>
2023-04-01 01:05:10 +0900

    Add const qualifier

--- libc/include/string.h
+++ libc/include/string.h
@@ -9,7 +9,7 @@
 
 char *strcpy(char *dest, const char *src);
 void *memset(void *s, int c, size_t sz);
-size_t strlen(char *s);
+size_t strlen(const char *s);
 void *memcpy(void *dest, const void *src, size_t n);
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
--- libc/string/strlen.c
+++ libc/string/strlen.c
@@ -1,7 +1,7 @@
 #include <string.h>
 
-size_t strlen(char *s) {
-    char *p = s;
+size_t strlen(const char *s) {
+    const char *p = s;
     size_t cnt = 0;
 
     while (*p != '\0') {

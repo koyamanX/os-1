caa37bc15806896dd4a327fb345096e98dada508
koyamanX <ckoyama.1996@gmail.com>
2023-04-08 00:31:03 +0900

    WIP Move userland address space to reserve first page

--- kernel/proc.c
+++ kernel/proc.c
@@ -36,13 +36,13 @@ void userinit(void) {
     pid = newproc();
 
     // Load initcode at address 0.
-    kvmmap(procs[pid].pgtbl, 0x0, (u64)init, PAGE_SIZE,
+    kvmmap(procs[pid].pgtbl, 4096, (u64)init, PAGE_SIZE,
            PTE_V | PTE_W | PTE_R | PTE_X | PTE_U);
 
     // Dump proc memory space.
     kvmdump(procs[pid].pgtbl, TRAPFRAME);
     kvmdump(procs[pid].pgtbl, TRAMPOLINE);
-    kvmdump(procs[pid].pgtbl, 0x0);
+    kvmdump(procs[pid].pgtbl, 4096);
 }
 
 int newproc(void) {
@@ -69,7 +69,7 @@ found:
 
     // Initialize trapframe.
     memset(p->tf, 0, sizeof(trapframe_t));
-    p->tf->sepc = 0x0;
+    p->tf->sepc = 4096;
 
     // Initialize context.
     p->ctx.ra = (u64)usertrapret;
@@ -77,6 +77,7 @@ found:
     p->tf->satp = SATP(p->pgtbl);
     p->tf->ksp = (u64)(p->kstack + PAGE_SIZE);
     p->tf->trap_handler = (u64)(kerneltrap);
+    p->tf->sp = 4096 + PAGE_SIZE;
     // Map trapframe and trampoline to proc's memory space.
     kvmmap(p->pgtbl, TRAPFRAME, (u64)p->tf, PAGE_SIZE, PTE_V | PTE_W | PTE_R);
     kvmmap(p->pgtbl, TRAMPOLINE, (u64)trampoline, PAGE_SIZE,
@@ -120,7 +121,7 @@ int exec(const char *file, char const **argv) {
                 "%x,p_memsz: %x, p_align: %x\n",
                 phdr[i].p_offset, phdr[i].p_vaddr, phdr[i].p_paddr,
                 phdr[i].p_filesz, phdr[i].p_memsz, phdr[i].p_align);
-            seg = (char *)va2pa(rp->pgtbl, 0x0);
+            seg = (char *)va2pa(rp->pgtbl, 4096);
             memset(seg, 0, PAGE_SIZE);
             readi(ip, (char *)seg, phdr[i].p_offset, PAGE_SIZE);
             sfence_vma();  // required?
@@ -131,7 +132,7 @@ int exec(const char *file, char const **argv) {
     kfree(phdr);
 
     rp->tf->sepc = ehdr.e_entry;
-    rp->tf->sp = PAGE_SIZE;
+    rp->tf->sp = 4096 + PAGE_SIZE;
 
     return 0;
 }
@@ -158,7 +159,7 @@ int fork(void) {
     // Set return value for parent.
     parent->tf->a0 = pid;
     // Copy memory space.
-    uvmcopy(child->pgtbl, parent->pgtbl, PAGE_SIZE);
+    uvmcopy(child->pgtbl, parent->pgtbl, PAGE_SIZE * 2);
     // Copy open files.
     memcpy((char *)child->ofile, (char *)parent->ofile, sizeof(parent->ofile));
 

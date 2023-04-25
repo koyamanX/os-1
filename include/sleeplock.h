1de0fca600e39860f1272b112e643cedad5dca6a
koyamanX <ckoyama.1996@gmail.com>
2023-04-26 01:53:53 +0900

    WIP Impl sleeplock

---
+++ include/sleeplock.h
@@ -0,0 +1,15 @@
+#ifndef _SLEEPLOCK_H
+#define _SLEEPLOCK_H
+
+struct sleeplock {
+    int locked;
+    int pid;
+    char *name;
+};
+
+void sleep_lock_init(struct sleeplock *lk, char *name);
+void sleep_lock(struct sleeplock *lk);
+void sleep_unlock(struct sleeplock *lk);
+int sleep_is_lock(struct sleeplock *lk);
+
+#endif /* _SLEEPLOCK_H */
---
+++ kernel/sleeplock.c
@@ -0,0 +1,25 @@
+#include <proc.h>
+#include <sleeplock.h>
+
+void sleep_lock_init(struct sleeplock *lk, char *name) {
+    lk->name = name;
+    lk->locked = 0;
+    lk->pid = 0;
+}
+
+void sleep_lock(struct sleeplock *lk) {
+    while (__sync_lock_test_and_set(&lk->locked, 1) != 0) {
+        sleep(lk);
+    }
+    lk->pid = this_proc()->pid;
+}
+
+void sleep_unlock(struct sleeplock *lk) {
+    __sync_lock_release(&lk->locked);
+    lk->pid = 0;
+    wakeup(lk);
+}
+
+int sleep_is_lock(struct sleeplock *lk) {
+    return lk->locked && (lk->pid == this_proc()->pid);
+}

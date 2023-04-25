6ec6e78d7d4ec995cd5588ba857e0d8307ee3cb5
koyamanX <ckoyama.1996@gmail.com>
2023-04-25 22:48:52 +0900

    Fix toolchain file

--- riscv.cmake
+++ riscv.cmake
@@ -3,6 +3,7 @@ set(TOOLCHAIN_PREFIX "riscv64-linux-gnu-")
 set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc${TOOLCHAIN_SUFFIX}")
 set(CMAKE_ASM_COMPILER "${TOOLCHAIN_PREFIX}gcc${TOOLCHAIN_SUFFIX}")
 set(CMAKE_LINKER "${TOOLCHAIN_PREFIX}gcc${TOOLCHAIN_SUFFIX}")
-set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wall -Werror -O0 -ggdb -g3 -gdwarf-2 -march=rv64g -ffreestanding -fno-common -nostdlib")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -Wall -Werror -O0 -ggdb -g3 -gdwarf-2 -march=rv64g -ffreestanding -fno-common -nostdlib")
-
+set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -Wall -Werror -O0 -ggdb -g3 -gdwarf-2 -march=rv64g -ffreestanding -fno-common -nostdlib \
+						-fno-omit-frame-pointer -mno-relax -mcmodel=medany -fno-stack-protector -fno-pie -no-pie")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -Wall -Werror -O0 -ggdb -g3 -gdwarf-2 -march=rv64g -ffreestanding -fno-common -nostdlib \
+						-fno-omit-frame-pointer -mno-relax -mcmodel=medany -fno-stack-protector -fno-pie -no-pie")

535a290ce09a02ee90ad27f254cc0388ee2f5b8b
koyamanX <ckoyama.1996@gmail.com>
2023-05-03 02:01:11 +0900

    Add simple ls

--- userland/ls.c
+++ userland/ls.c
@@ -5,38 +5,81 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#define DIRSIZ 60
+char *fmtname(char *path) {
+    static char buf[DIRSIZ + 1];
+    char *p;
+
+    // Find first character after last slash.
+    for (p = path + strlen(path); p >= path && *p != '/'; p--)
+        ;
+    p++;
+
+    // Return blank-padded name.
+    if (strlen(p) >= DIRSIZ)
+        return p;
+    memmove(buf, p, strlen(p));
+    memset(buf + strlen(p), ' ', DIRSIZ - strlen(p));
+    return buf;
+}
+
+struct dirent {
+    unsigned int ino;   //!< Inode number this direct entry associated with.
+    char name[DIRSIZ];  //!< Name of file.
+};
+
 void ls(char *path) {
+    char buf[512], *p;
     int fd;
+    struct dirent de;
     struct stat st;
 
-    if ((fd = open(path, O_RDONLY, 0)) < 0) {
-        write(1, "err:1\n", 6);
+    if ((fd = open(path, 0)) < 0) {
+        fprintf(stderr, "ls: cannot open %s\n", path);
         return;
     }
 
     if (fstat(fd, &st) < 0) {
-        write(1, "err:2\n", 6);
+        fprintf(stderr, "ls: cannot stat %s\n", path);
         close(fd);
         return;
     }
 
     switch (st.st_mode & S_IFMT) {
-        case S_IFBLK:
-        case S_IFCHR:
         case S_IFREG:
-            // printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
-            write(1, path, strlen(path));
-            write(1, "\n", 1);
+            printf("%s %d %d %lx\n", fmtname(path), st.st_mode, st.st_ino,
+                   st.st_size);
+            break;
+
+        case S_IFDIR:
+            if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
+                printf("ls: path too long\n");
+                break;
+            }
+            strcpy(buf, path);
+            p = buf + strlen(buf);
+            *p++ = '/';
+            while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+                if (de.ino == 0)
+                    continue;
+                memmove(p, de.name, DIRSIZ);
+                p[DIRSIZ] = 0;
+                if (stat(buf, &st) < 0) {
+                    printf("ls: cannot stat %s\n", buf);
+                    continue;
+                }
+                printf("%s %d %d %lx\n", fmtname(buf), st.st_mode, st.st_ino,
+                       st.st_size);
+            }
             break;
         default:
-            write(1, "err:3\n", 6);
             break;
     }
     close(fd);
 }
 
 int main(int argc, char *argv[]) {
-    write(1, "hello", 5);
+    ls("/");
 
     return 0;
 }

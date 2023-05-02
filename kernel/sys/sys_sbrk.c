e836b49337001fdf858e305eed53a519830c3122
koyamanX <ckoyama.1996@gmail.com>
2023-05-02 13:17:09 +0900

    Refactor

--- kernel/sys/sys_sbrk.c
+++ kernel/sys/sys_sbrk.c
@@ -1,18 +1,24 @@
+#include <printk.h>
 #include <proc.h>
 #include <vm.h>
 
-void *sbrk(intptr_t increment) {
+void *sbrk(ptrdiff_t increment) {
     struct proc *rp;
     void *addr;
 
     rp = this_proc();
-    addr = (void *)rp->heap;
-    for (u64 i = 0; i < ROUNDUP(increment); i -= PAGE_SIZE) {
-        kvmmap(rp->pgtbl, rp->heap + i, (u64)alloc_page(), PAGE_SIZE,
-               PTE_V | PTE_W | PTE_R | PTE_X | PTE_U);
-        kvmdump(rp->pgtbl, rp->heap + i);
+    addr = (void *)ROUNDDOWN((void *)rp->heap);
+    for (u64 i = 0; i < increment; i += PAGE_SIZE) {
+        pte_t *pte;
+
+        pte = kvmwalk(rp->pgtbl, (u64)(addr + i));
+        if (*pte == 0) {
+            kvmmap(rp->pgtbl, (u64)(addr + i), (u64)alloc_page(), PAGE_SIZE,
+                   PTE_V | PTE_W | PTE_R | PTE_X | PTE_U);
+        }
+        kvmdump(rp->pgtbl, (u64)(addr + i));
     }
-    rp->heap = ROUNDUP(increment);
+    rp->heap = rp->heap + increment;
 
     return addr;
 }
